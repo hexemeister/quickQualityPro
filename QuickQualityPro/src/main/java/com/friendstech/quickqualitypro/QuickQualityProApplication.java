681c3ab41db72125d6a9f3d938b4309985cbdd0d
Renato Moraes <Luiz@192.168.15.4>
2018-12-24 12:59:26 -0200

    Primeiro commit primeiras configurações

---
+++ QuickQualityPro/src/main/java/com/friendstech/quickqualitypro/QuickQualityProApplication.java
@@ -0,0 +1,14 @@
+package com.friendstech.quickqualitypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickQualityProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickQualityProApplication.class, args);
+	}
+
+}
+
